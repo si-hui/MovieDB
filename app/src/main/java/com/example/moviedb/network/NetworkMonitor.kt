f20e1cfffafe821c6f2e63ca1b55376adf3f83a7
larissa-1101 <larissa.tay220203@gmail.com>
2026-05-04 03:42:45 +0200

    Add NetworkMonitor to detect internet connectivity changes

---
+++ app/src/main/java/com/example/moviedb/network/NetworkMonitor.kt
@@ -0,0 +1,39 @@
+package com.example.moviedb.network
+
+import android.content.Context
+import android.net.ConnectivityManager
+import android.net.Network
+import android.net.NetworkCapabilities
+import android.net.NetworkRequest
+import kotlinx.coroutines.channels.awaitClose
+import kotlinx.coroutines.flow.callbackFlow
+
+class NetworkMonitor(private val context: Context) {
+    fun observeNetworkState() = callbackFlow {
+        val connectivityManager = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+
+        val callback = object : ConnectivityManager.NetworkCallback() {
+            override fun onAvailable(network: Network) {
+                trySend(true)
+            }
+
+            override fun onLost(network: Network) {
+                trySend(false)
+            }
+        }
+
+        val networkRequest = NetworkRequest.Builder()
+            .addCapability(NetworkCapabilities.NET_CAPABILITY_INTERNET)
+            .build()
+
+        connectivityManager.registerNetworkCallback(networkRequest, callback)
+
+        // Send initial state
+        val isConnected = connectivityManager.activeNetwork != null
+        trySend(isConnected)
+
+        awaitClose {
+            connectivityManager.unregisterNetworkCallback(callback)
+        }
+    }
+}
